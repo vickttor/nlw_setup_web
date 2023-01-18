c615822d6f29b15c8f7a69bc95bf9e4e8a5539db
Victor Silva <victor.silvaPC@hotmail.com>
2023-01-18 19:49:51 -0300

    :zap: feat: added styles and components in home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { Habit } from "./components/Habit"
 import "./styles/global.css"
+import { Header } from "./components/Header"
+import { SummaryTable } from "./components/SummaryTable"
 
 export function App() {
   return (
-    <>
-      <Habit completed={10}/>
-      <Habit completed={3}/>
-      <Habit completed={5}/>
-      <Habit completed={9}/>
-      <Habit completed={2}/>
-    </>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header/>
+        <SummaryTable/>
+      </div>
+    </div>
   )
 }
