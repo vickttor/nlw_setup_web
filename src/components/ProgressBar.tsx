03093cec4fca56b37079bde1c120771637cdb690
Victor Silva <victor.silvaPC@hotmail.com>
2023-01-19 22:35:15 -0300

    :zap: feat: added dialog and popover component with Radix | using clsx to use conditional attribute values

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,29 @@
+import * as Progress from '@radix-ui/react-progress'
+
+interface ProgressBarProps {
+  progress: number
+}
+
+export function ProgressBar({progress, ...props}: ProgressBarProps) {
+
+  return (
+    <Progress.Root className='h-3 rounded-xl bg-zinc-700 w-full mt-4' value={progress}>
+      <Progress.Indicator
+        className="h-3 rounded-xl bg-violet-600"
+        style={{width: `${progress}%`}}
+      />
+    </Progress.Root>
+  )
+}
+
+// Native Way:
+
+{/* <div className='h-3 rounded-xl bg-zinc-700 w-full mt-4'>
+      <div 
+        role="progressbar"
+        aria-label="Progresso de hábitos completados nesse dia"
+        aria-valuenow={75}
+        className='h-3 rounded-xl bg-violet-600 w-3/4'>
+      </div>
+    </div> 
+*/}
